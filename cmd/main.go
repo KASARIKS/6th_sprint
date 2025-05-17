dff3754e35ae6e053d3e49a364b1bc29c3f67aa5
Golovko, Yaroslav <golovko085@gmail.com>
2025-05-17 14:35:10 +0700

    develop not-ended functional in handlers

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/handlers"
+)
+
+func main() {
+	http.HandleFunc("/", handlers.MainHandler)
+	http.HandleFunc("/upload", handlers.UploadHandler)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,37 @@
 package handlers
+
+import (
+	"io"
+	"net/http"
+	"os"
+)
+
+func MainHandler(w http.ResponseWriter, r *http.Request) {
+	resp, err := readPageFile()
+	if err != nil {
+		http.Error(w, "Internal problems! Try later or contact support.", http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(resp)
+}
+
+func readPageFile() ([]byte, error) {
+	page, err := os.ReadFile("index.html")
+	return page, err
+}
+
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Wrong method!", http.StatusBadRequest)
+		return
+	}
+
+	_, header, err := r.FormFile("myFile")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	io.WriteString(w, header.Filename)
+}
