1cf0532920550d3cbe7495c28ccc19b0722e0d7f
Golovko, Yaroslav <golovko085@gmail.com>
2025-05-17 13:58:16 +0700

    develop service module

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,47 @@
 package service
+
+import (
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func Convert(text string) string {
+	convText := newConvertedText(text)
+	return convText.res
+}
+
+type convertedText struct {
+	sourceText  string
+	checkString string
+	res         string
+}
+
+func newConvertedText(text string) *convertedText {
+	convText := &convertedText{
+		sourceText: text,
+	}
+
+	convText.convertText()
+
+	return convText
+}
+
+func (convText *convertedText) convertText() {
+	convText.makeCheckString()
+	convText.makeRes()
+}
+
+func (convText *convertedText) makeCheckString() {
+	convText.checkString = strings.Replace(convText.sourceText, ".", "", -1)
+	convText.checkString = strings.Replace(convText.checkString, "-", "", -1)
+	convText.checkString = strings.Replace(convText.checkString, " ", "", -1)
+}
+
+func (convText *convertedText) makeRes() {
+	if convText.checkString == "" {
+		convText.res = morse.ToText(convText.sourceText)
+	} else {
+		convText.res = morse.ToMorse(convText.sourceText)
+	}
+}
